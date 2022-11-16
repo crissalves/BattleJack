9595db9237838b0c4fd8d4f142ea438255bc6e51
crissalves <cristianalves_cg@hotmail.com>
2022-11-16 10:49:39 -0300

    Possivel Menu

--- main.hs
+++ main.hs
@@ -1,4 +1,44 @@
 main :: IO()
 main = do
-    entrada <- getLine
-    print ("Olá " ++ entrada)
+    putStrLn "Boas vindas ao Battlejack !"
+    menuInicial
+
+
+menuInicial :: IO()
+menuInicial = do
+    putStrLn "\nMenu Inicial \n"
+    putStrLn "1 -> Iniciar Jogo "
+    putStrLn "2 -> Regras "
+    putStrLn "3 -> Créditos "
+    putStrLn "4 -> Sair "
+    putStrLn "\nSelecione o número da opção desejada: "
+    opcao <- getLine
+    escolha(read opcao :: Int)
+
+
+--Resolver o prelude.read: no parse para não quebrar o programa
+escolha :: Int -> IO()
+escolha opcao 
+            | opcao == 1 = putStrLn ("Jogo")
+            | opcao == 2 = regras
+            | opcao == 3 = creditos
+            | opcao == 4 = putStrLn ("Xau ;) volte logo")
+            | otherwise = do 
+                putStrLn ("Escolha uma opção válida >:(" )
+                menuInicial
+
+
+regras :: IO()
+regras = do
+    putStrLn "\nRegras : \n"
+    putStrLn "Pressione Enter para voltar ao menu Inicial."
+    x <- getLine
+    menuInicial
+
+
+creditos :: IO()
+creditos = do
+    putStrLn "Pessoas : \n"
+    putStrLn "Pressione Enter para voltar ao menu Inicial"
+    x <- getLine
+    menuInicial
