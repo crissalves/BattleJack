83557a3894f4a5f82900eaffc34fddc604e6c990
Tampasco <lucas.carvalho@ccc.ufcg.edu.br>
2023-01-29 12:29:34 -0300

    Estrutura inicial

--- Logico/jogo.pl
+++ Logico/jogo.pl
@@ -1,19 +1,17 @@
-adicionarCarta([],Carta,[Carta]).
-adicionarCarta(Mao,Carta,Rmao):- Mao = [H|T],Rmao = [H|[Carta|T]].
+jogoVSjogador:-
+	write("Jogador 1 digite seu nome: "),
+	read_line_to_string(user_input,X),
+	write("Jogador 2 digite seu nome: "),
+	read_line_to_string(user_input,Y),
+	criaJogador(X),nl,
+	criaJogador(Y),
+	iniciarRodada(X,Y).
 
-jogador1("Lucas",100,[],false).
-jogador2("Cris",100,[],false).
+iniciarRodada(J1,J2):-
+	jogador(J1,_,M1,_),
+	jogador(J2,_,M2,_),
 
-mostrarMao([]).
-mostrarMao(Lista):- Lista = [H|T], H = [X,Y], write(X), write(" de "), write(Y),nl, mostrarMao(T).
-
-pedirCarta(Nums, Naipes, Carta):- random(0,12,X), nth0(X,Nums,R), random(0,3,Y), nth0(Y,Naipes,R1), Carta = [R,R1].
-
-iniciarRodada:-
-	jogador1("Lucas",V1,M1,P1),
-	jogador2("Cris",V2,M2,P2),
-	Nums = ["A","2","3","4","5","6","7","8","9","10","Valete","Rainha", "Rei"],
-	Naipes = ["Copas","Paus", "Ouros", "Espadas"],
+	baralho(Nums,Naipes),
 	pedirCarta(Nums, Naipes, Carta),
 	pedirCarta(Nums, Naipes, Carta2),
 	pedirCarta(Nums, Naipes, Carta3),
@@ -22,11 +20,38 @@ iniciarRodada:-
 	adicionarCarta(M2,Carta2,R2),
 	adicionarCarta(R1,Carta3,R3),
 	adicionarCarta(R2,Carta4,R4),
+
+	atualizaMao(J1, R3),
+	atualizaMao(J2, R4),
 	write("As cartas de J1 sao:"),
 	nl,mostrarMao(R3),
 	nl,
 	write("As cartas do J2 sao:"),
-	nl,mostrarMao(R4).
+	nl,mostrarMao(R4),
+	nl,write("Aperte Enter para continuar:"),
+	read_line_to_string(user_input,X),nl,
+	batalhaVSjogador(J1,J2).
+
+batalhaVSjogador(J1,J2):-
+	write("A fazer...").
+
+
+baralho(["As","2","3","4","5","6","7","8","9","10","Valete","Rainha", "Rei"],["Copas","Paus", "Ouros", "Espadas"]).
+
+adicionarCarta([],Carta,[Carta]).
+adicionarCarta(Mao,Carta,Rmao):- Mao = [H|T],Rmao = [H|[Carta|T]].
+
+mostrarMao([]).
+mostrarMao(Lista):- Lista = [H|T], H = [X,Y], write(X), write(" de "), write(Y),nl, mostrarMao(T).
+
+pedirCarta(Nums, Naipes, Carta):- random(0,12,X), nth0(X,Nums,R), random(0,3,Y), nth0(Y,Naipes,R1), Carta = [R,R1].
+
+criaJogador(Nome):- assert(jogador(Nome,100,[],false)).
+atualizaMao(Nome,Cartas):- retract(jogador(Nome,X,_,Y)), assert(jogador(Nome,X,Cartas,Y)).
+
+:- dynamic jogador/4.
+
+
 
 	
 	
